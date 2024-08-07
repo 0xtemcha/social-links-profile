91e7f6b2ab5c396e0542b6d07ce140407bd84cb0
temcha <itemchur@spin.ai>
2024-08-07 17:04:00 +0300

    added style guide, fonts, basic styles, assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <>App</>;
+  return (
+    <>
+      Jessica Randall London, United Kingdom "Front-end developer and avid
+      reader." GitHub Frontend Mentor LinkedIn Twitter Instagram
+    </>
+  );
 }
 
 export default App;
